8c58c4bc0eae0613620dce276807e409657cc357
pakiZBRG <pavlovicnikola511@gmail.com>
2020-07-11 09:34:40 +0200

    v2.1.1

--- src/components/Home/Navbar.js
+++ src/components/Home/Navbar.js
@@ -6,12 +6,12 @@ export default function NavBar() {
     return(
         <div className='container-fluid py-2 d-flex justify-content-center navbar'>
             <div className='row'>
-                <div className='col-lg-6 text-center'>
-                <Link to={'/'}>
-                    <h3 className='pt-1'>Paki's Movie</h3>
-                </Link>
+                <div className='col-12 text-center'>
+                    <h3 className='pt-1'>
+                        <Link to={'/'}>Paki's Movie</Link>
+                    </h3>
                 </div>
-                <div className='col-lg-6'>
+                <div className='col-12 pt-1'>
                     <MovieSearch/>
                 </div>
             </div>
