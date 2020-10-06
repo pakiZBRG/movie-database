82efb0fcaf4003630d6b11ea0fc44c2538c7f96b
unknown <pavlovicnikola511@gmail.com>
2020-10-06 13:52:07 +0200

    retrun function

--- src/components/MovieDetail/Casts.js
+++ src/components/MovieDetail/Casts.js
@@ -9,17 +9,6 @@ export default function Casts({params}){
         fetchAPI()
     }, [params.id])
 
-    const castList = casts.map((cast, i) => {
-        const {name, character, img} = cast
-        return (
-            <div className='col-lg-2 col-sm-4' key={i}>
-                <img className='cast-img d-flex mx-auto' src={img} alt={name}/>
-                <h5 className='text-center my-2'>{name}</h5>
-                <p className='text-center text-secondary mb-4'>{character}</p>
-            </div>
-        )
-    })
-
     return(
         <div className='container-b mx-auto'>
             <div className='row mt-4'>
@@ -27,7 +16,19 @@ export default function Casts({params}){
                     <h2 className='text-center mb-4' style={{color: '#5a606b'}}>Cast</h2>
                 </div>
             </div>
-            <div className='row mt-2'>{castList}</div>
+            <div className='row mt-2'>
+                {casts.map((cast, i) => {
+                    const {name, character, img} = cast
+                    if(img !== 'https://image.tmdb.org/t/p/original/null')
+                        return (
+                            <div className='col-lg-2 col-sm-4' key={i}>
+                                <img className='cast-img d-flex mx-auto' src={img} alt={name}/>
+                                <h5 className='text-center my-2'>{name}</h5>
+                                <p className='text-center text-secondary mb-4'>{character}</p>
+                            </div>
+                        )
+                })}
+            </div>
         </div>
     )
 }
